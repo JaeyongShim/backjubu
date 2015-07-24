205c9e13b44d295e7069efa559fea04ee60bcd01
jaeyong <uc011@hanmail.net>
2015-07-24 07:49:56 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Backjubu::Application.config.secret_key_base = '2b731fd1d8b4a3ba6c449d1f5469188e7ea7d83427708f4296a1255237d3ec6a058287b342effaed8a84be58f965636518e1c5850cf92a6cd3274aaa8ed4347b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Backjubu::Application.config.session_store :cookie_store, key: '_backjubu_session'
