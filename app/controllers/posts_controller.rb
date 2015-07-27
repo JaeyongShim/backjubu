6b09d43e49a646f5f0a27e9d7baa7da3fd542b40
jaeyong <uc011@hanmail.net>
2015-07-27 10:34:27 +0000

    show_category 와 index views에 중복되는 코드 처리

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
-		@posts=Post.all
+		@posts=Post.reverse.all
   end
 
   def show
+		@post=Post.find(params[:id])
   end
 
   def show_category
